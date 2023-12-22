5251bdc7b9efa7c9daaa65a1c478e02625f5edf8
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-12-22 16:33:16 +0300

    Create duration_02.cpp

---
+++ duration/duration_02.cpp
@@ -0,0 +1,19 @@
+#include <chrono>
+#include <iostream>
+
+int main()
+{
+	using namespace std::chrono;
+
+	//seconds s1 = 345; //gecersiz
+	seconds s2(345);
+	std::cout << "s2.count() = " << s2.count() << "\n";
+	seconds s3{ 761 };
+	std::cout << "s3.count() = " << s3.count() << "\n";
+	seconds s4{};
+	std::cout << "s4.count() = " << s4.count() << "\n";
+	//seconds s5;
+	//std::cout << "s5.count() = " << s5.count() << "\n"; //ub
+	seconds s6 = 675s;
+	std::cout << "s6.count() = " << s6.count() << "\n"; //ub
+}
