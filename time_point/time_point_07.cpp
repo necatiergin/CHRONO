c7eee9d5b1791c232dc4fff9648bc43fb738d095
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-12-22 17:13:36 +0300

    Create time_point_07.cpp

---
+++ time_point/time_point_07.cpp
@@ -0,0 +1,16 @@
+// örnek cppreference.com sitesinden alınmıştır
+
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <chrono>
+
+int main()
+{
+    //std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
+    auto now = std::chrono::system_clock::now();
+
+    std::time_t now_c = std::chrono::system_clock::to_time_t(now - std::chrono::hours(24));
+    std::cout << "One day ago, the time was "
+        << std::put_time(std::localtime(&now_c), "%F %T") << '\n';
+}
