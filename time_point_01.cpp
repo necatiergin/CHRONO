e0a53afdf97cd984cf468addd8fd747c8a1e595f
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-12-22 16:54:47 +0300

    Create time_point_01.cpp

---
+++ time_point_01.cpp
@@ -0,0 +1,28 @@
+#include <chrono>
+#include <vector>
+#include <random>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+using namespace chrono;
+
+int main()
+{
+	constexpr size_t size{ 1'000'000 };
+	vector<int> ivec(size);
+
+	mt19937 eng;
+	uniform_int_distribution dist{ 0, 1'000'000 };
+
+	const auto tp_start = steady_clock::now();
+
+	generate(ivec.begin(), ivec.end(), [&] {return dist(eng); });
+	sort(ivec.begin(), ivec.end());
+
+	const auto tp_end = steady_clock::now();
+	cout << "islem sona erdi\n";
+
+	cout << "sure : " << duration_cast<milliseconds>(tp_end - tp_start).count() << " milliseconds\n";
+	cout << "sure : " << duration<double>(tp_end - tp_start).count() << " seconds\n";
+}
