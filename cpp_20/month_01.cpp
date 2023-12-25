ad1f5188253c51fe7603d57f423240511a19e716
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-12-25 17:35:21 +0300

    Create month_01.cpp

---
+++ cpp_20/month_01.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <chrono>
+
+int main()
+{
+    using namespace std;
+    using namespace chrono;
+
+    cout << January << '\n';
+    cout << February << '\n';
+    cout << March << '\n';
+    cout << April << '\n';
+    cout << May << '\n';
+    cout << June << '\n';
+    cout << July << '\n';
+    cout << August << '\n';
+    cout << September << '\n';
+    cout << October << '\n';
+    cout << November << '\n';
+    cout << December << '\n';
+}
