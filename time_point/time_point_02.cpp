2777575241793ebc27715e2efa55f07f4cb4520d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-06-29 07:35:45 +0300

    Update time_point_02.cpp

--- time_point/time_point_02.cpp
+++ time_point/time_point_02.cpp
@@ -2,13 +2,15 @@
 #include <iostream>
 #include <ctime>
 
-using namespace std;
-using namespace chrono;
-
 int main()
 {
+	using namespace std;
+	using namespace chrono;
+	
 	system_clock::time_point tpx; //default ctor (epoche)
 
 	auto timer = system_clock::to_time_t(tpx);
 	cout << ctime(&timer);
+
+	cout << tpx << '\n'; //C++20
 }
