8120518a1c1911dacd32889d3f028cabe09736a4
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-01-22 17:20:24 +0300

    Update duration_03.cpp

--- duration/duration_03.cpp
+++ duration/duration_03.cpp
@@ -3,25 +3,26 @@
 #include <vector>
 #include <algorithm>
 
-using namespace std;
-using namespace chrono;
-
-vector<int> spend_time(size_t n)
+std::vector<int> spend_time(std::size_t n)
 {
-	vector<int> ivec(n);
-	generate(begin(ivec), end(ivec), rand);
-	sort(begin(ivec), end(ivec));
+	std::vector<int> ivec(n);
+	generate(begin(ivec), end(ivec), std::rand); // adl
+	sort(begin(ivec), end(ivec)); //adl
 
 	return ivec;
 }
 
 int main()
 {
+	using namespace std;
+	using namespace std::chrono;
+
+
 	auto tp_start = steady_clock::now();
 	auto x = spend_time(30'000);
 	auto tp_end = steady_clock::now();
 
-	cout << nanoseconds{ tp_end - tp_start }.count() << " nanosaniye\n";
-	cout << duration_cast<microseconds>(tp_end - tp_start).count() << " mikrosaniye\n";
-	cout << duration_cast<milliseconds>(tp_end - tp_start).count() << " milisaniye\n";
+	cout << nanoseconds{ tp_end - tp_start }.count() << " nanoseconds\n";
+	cout << duration_cast<microseconds>(tp_end - tp_start).count() << " microseconds\n";
+	cout << duration_cast<milliseconds>(tp_end - tp_start).count() << " milliseconds\n";
 }
