e067fe653a6890092ab58ef5675fbc81a57b0c89
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-01-22 18:19:42 +0300

    Update time_point_10.cpp

--- time_point/time_point_10.cpp
+++ time_point/time_point_10.cpp
@@ -5,7 +5,7 @@
 int main()
 {
 	using xtype = std::chrono::system_clock::time_point;
-	using ytype = std::chrono::time_point< std::chrono::system_clock>;
+	using ytype = std::chrono::time_point<std::chrono::system_clock>;
 
 	constexpr auto b = std::is_same_v<xtype, ytype>;  //true
 }
