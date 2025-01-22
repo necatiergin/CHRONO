c891b99412d13983867d95535cc7d54fbf9d5171
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-01-22 18:15:06 +0300

    Update time_point_04.cpp

--- time_point/time_point_04.cpp
+++ time_point/time_point_04.cpp
@@ -1,11 +1,9 @@
 #include <chrono>
 #include <random>
 
-using namespace std;
-using namespace chrono;
-
 int main()
 {
-	mt19937 eng(static_cast<unsigned>(duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count()));
-        //...
+	using namespace std::chrono;
+	std::mt19937 eng(static_cast<unsigned>(duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count()));
+	//...
 }
