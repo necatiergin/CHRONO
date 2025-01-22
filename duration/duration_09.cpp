7487f477e1aa9484fe4bd6bdf7f31f19a6f7164e
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-01-22 17:34:29 +0300

    Update duration_09.cpp

--- duration/duration_09.cpp
+++ duration/duration_09.cpp
@@ -7,25 +7,24 @@ std::ostream& operator<<(std::ostream& os, const std::chrono::duration<R, P>& du
 	return os << dur.count() << " * (" << P::num << " / " << P::den << ")";
 }
 
-using namespace std;
-using namespace chrono;
-
-using halfsec = std::chrono::duration<long long, std::ratio<1, 2>>;
-using frame = std::chrono::duration<double, std::ratio<1, 10>>;
-
-using Day = std::chrono::duration<int, std::ratio<60 * 60 * 24>>;
-
 int main()
 {
+	using namespace std::chrono;
+
+	using halfsec = duration<long long, std::ratio<1, 2>>;
+	using frame = duration<double, std::ratio<1, 10>>;
+
+	using Day = duration<int, std::ratio<60 * 60 * 24>>;
+
 	halfsec x{ 128 };
 
 	++x;
 	++x;
 	++x;
 
-	cout << x.count() << '\n';
-	cout << milliseconds{ x }.count() << '\n';
-	cout << frame{ x }.count() << '\n';
-	cout << duration_cast<Day>(x).count() << '\n';
-	cout << x << '\n' << milliseconds{ x } << '\n';
+	std::cout << x.count() << '\n';
+	std::cout << milliseconds{ x }.count() << '\n';
+	std::cout << frame{ x }.count() << '\n';
+	std::cout << duration_cast<Day>(x).count() << '\n';
+	std::cout << x << '\n' << milliseconds{ x } << '\n';
 }
