14c7e72ec2b1057af31f2f13e43337f7a5162ef5
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2026-02-03 14:59:21 +0300

    Update clock_06.cpp

--- clock/clock_06.cpp
+++ clock/clock_06.cpp
@@ -4,15 +4,14 @@
 
 int main()
 {
-	using namespace std;
-	using namespace chrono;
-	
+	using namespace std::chrono;
+
 	//using clock = system_clock;
 	//using clock = steady_clock;
 	using clock = high_resolution_clock;
 
-	cout << "period     =  " << typeid(clock::period).name() << '\n';
-	cout << "duration   =  " << typeid(clock::duration).name() << '\n';
-	cout << "rep        =  " << typeid(clock::rep).name() << '\n';
-	cout << "time_point =  " << typeid(clock::time_point).name() << '\n';
+	std::cout << "period     =  " << typeid(clock::period).name() << '\n';
+	std::cout << "duration   =  " << typeid(clock::duration).name() << '\n';
+	std::cout << "rep        =  " << typeid(clock::rep).name() << '\n';
+	std::cout << "time_point =  " << typeid(clock::time_point).name() << '\n';
 }
