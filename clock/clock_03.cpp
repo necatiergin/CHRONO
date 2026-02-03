a1116e5d44be10773298525e9ed9506bddb4667a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2026-02-03 14:53:10 +0300

    Update clock_03.cpp

--- clock/clock_03.cpp
+++ clock/clock_03.cpp
@@ -1,13 +1,23 @@
-#include <iostream>
 #include <chrono>
-#include <typeinfo>
+#include <ctime>
+#include <iomanip>
+#include <iostream>
 
 int main()
 {
-	using namespace std::chrono;
-	using std::cout;
+    using namespace std::chrono;
 
-	cout << typeid(system_clock::time_point).name() << '\n';
-	cout << typeid(high_resolution_clock::time_point).name() << '\n';
-	cout << typeid(steady_clock::time_point).name() << '\n';
+    system_clock::time_point now = system_clock::now();
+    minutes offset{ 113000 };
+    // past and future
+    system_clock::time_point past = now - offset;
+    system_clock::time_point future = now + offset;
+
+    std::time_t now_tt = system_clock::to_time_t(now);
+    std::time_t past_tt = system_clock::to_time_t(past);
+    std::time_t future_tt = system_clock::to_time_t(future);
+
+    std::cout << "Now     : " << std::put_time(localtime(&now_tt), "%Y-%m-%d %H:%M:%S") << '\n';
+    std::cout << "Past    : " << std::put_time(localtime(&past_tt), "%Y-%m-%d %H:%M:%S") << '\n';
+    std::cout << "Future  : " << std::put_time(localtime(&future_tt), "%Y-%m-%d %H:%M:%S") << '\n';
 }
