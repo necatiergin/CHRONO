719476cee759ed7990530b10968057baf1178b10
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2026-02-03 15:03:09 +0300

    Update duration_03.cpp

--- duration/duration_03.cpp
+++ duration/duration_03.cpp
@@ -14,15 +14,13 @@ std::vector<int> spend_time(std::size_t n)
 
 int main()
 {
-	using namespace std;
 	using namespace std::chrono;
-
 
 	auto tp_start = steady_clock::now();
 	auto x = spend_time(30'000);
 	auto tp_end = steady_clock::now();
 
-	cout << nanoseconds{ tp_end - tp_start }.count() << " nanoseconds\n";
-	cout << duration_cast<microseconds>(tp_end - tp_start).count() << " microseconds\n";
-	cout << duration_cast<milliseconds>(tp_end - tp_start).count() << " milliseconds\n";
+	ats::cout << nanoseconds{ tp_end - tp_start }.count() << " nanoseconds\n";
+	ats::cout << duration_cast<microseconds>(tp_end - tp_start).count() << " microseconds\n";
+	ats::cout << duration_cast<milliseconds>(tp_end - tp_start).count() << " milliseconds\n";
 }
